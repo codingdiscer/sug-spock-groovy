327e907940c64966c3512673e5caf2c3f6d40de9
Dan Dowma <dan.dowma@target.com>
2016-09-21 23:14:23 -0500

    Initial commit

---
+++ src/main/groovy/sug/spock/GroovyAppApplication.groovy
@@ -0,0 +1,12 @@
+package sug.spock
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GroovyAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run GroovyAppApplication, args
+	}
+}
---
+++ src/test/groovy/sug/spock/GroovyAppApplicationTests.groovy
@@ -0,0 +1,16 @@
+package sug.spock
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class GroovyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
